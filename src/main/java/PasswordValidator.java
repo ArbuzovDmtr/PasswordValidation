fed3cb262c1a762e8a48c958a348f10104595572
ArbuzovDmtr <arbuzov.dmtr@gmail.com>
2026-02-27 00:07:50 +0100

    Added Methods and Tests for MinLength, containsDigit, containsUpperAndLower, isCommonPassword, containsSpecialChar, isValid

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,14 +1,66 @@
 public final class PasswordValidator {
 
-    public static boolean hasMinLength(String password, int min){return true;};
+    public static boolean hasMinLength(String password, int min){
+        return password.length() >= min;
+    };
 
-    public static boolean containsDigit(String password){return true;};
-    public static boolean containsUpperAndLower(String password){return true;};
-    public static boolean isCommonPassword(String password){return true;}; // kleine interne Liste
+    public static boolean containsDigit(String password){
+        String number="0123456789";
+        for(int i=0; i<password.length();i++){
+            if(number.contains(""+password.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    };
+    public static boolean containsUpper(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if(Character.isUpperCase(password.charAt(i))){
+                return true;
+            }
+
+        } return false;
+    }
+    public static boolean containsLower(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if(Character.isLowerCase(password.charAt(i))){
+                return true;
+            }
+
+        } return false;
+    }
+    public static boolean containsUpperAndLower(String password){
+        return containsLower(password)&&containsUpper(password);};
+
+    public static boolean isCommonPassword(String password){
+        String [] ListOfCommonPasswords ={"123456","admin","12345678","password",
+                "admin123","root","passwort","abc123","qwerty","654321",
+                "87654321","123123","asdfgh","user","test"};
+        for(String commonPass:ListOfCommonPasswords){
+            if(commonPass.equals(password.toLowerCase())){
+                return true;
+            }
+        }
+        return false;
+        }; // kleine interne Liste
     // Bonus:
-    public static boolean containsSpecialChar(String password, String allowed){return true;};
+    public static boolean containsSpecialChar(String password){
+        String SpecialChar="!@#$%^&*()-_+={}[]|:;'<>,.?/";
+        for(int i=0; i<password.length();i++){
+            if(SpecialChar.contains(""+password.charAt(i))){
+                return true;
+            }
+        }
+
+        return false;};
+
+
     // Optionale Gesamtsicht:
-    public static boolean isValid(String password){return true;}; // nutzt die obenstehenden Checks
+    public static boolean isValid(String password){
+        return hasMinLength(password,8)&&containsDigit(password)
+                &&containsUpperAndLower(password)&&containsSpecialChar(password)
+                &&!isCommonPassword(password);}; // nutzt die obenstehenden Checks
+
 }
 
 
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,47 +1,92 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorTest {
 
 
     @ParameterizedTest
-    @CsvSource ({"qwertyui","8","True",
-                 "qwertyui","4","True",
-                 "qwertyui","12","False"})
+    @CsvSource ({"qwertyui,8,true",
+            "qwertyui,4,true",
+            "GSd21g666gs,8,true",
+            "aDDm11n66,8,true",
+            "qwertyui,12,false"})
     void hasMinLength_shouldReturnTrue_WhenPasswordMinimumSymbolHas(String txt, int numb, boolean check){
         assertEquals(PasswordValidator.hasMinLength(txt,numb),check);
     }
 
     @ParameterizedTest
-    @CsvSource ({"qwe3tyui","True",
-                 "qwerty7i","True",
-                 "qwertyui","False"})
+    @CsvSource ({"qwe3tyui,true",
+            "qwerty7i,true",
+            "GSd21@1gg_?s,true",
+            "aDDm11n66,true",
+            "qwertyui,false"})
     void containsDigit_shouldReturnTrue_WhenPasswordDigitHas(String txt, boolean check){
         assertEquals(PasswordValidator.containsDigit(txt),check);
     }
     @ParameterizedTest
-    @CsvSource ({"Qwe3tyui","True",
-                 "qwerTY7i","True",
-                 "qwertyui","False"})
+    @CsvSource ({"Qwe3tyui,true",
+            "qwerTY7i,true",
+            "GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "qwertyui,false"})
     void containsUpperAndLower_shouldReturnTrue_WhenPasswordUpperAndLowerLetterHas(String txt, boolean check){
         assertEquals(PasswordValidator.containsUpperAndLower(txt),check);}
 
     @ParameterizedTest
-    @CsvSource ({"password","True",
-                 "admin","True",
-                 "GSd21$@1gg_?s","False"})
+    @CsvSource ({"Qwe3tyui,true",
+            "qwerTY7i,true",
+            "GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "QWERTZUI,false"})
+    void containsLower_shouldReturnTrue_WhenPasswordLowerLetterHas(String txt, boolean check){
+        assertEquals(PasswordValidator.containsLower(txt),check);}
+    @ParameterizedTest
+    @CsvSource ({"Qwe3tyui,true",
+            "qwerTY7i,true",
+            "GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "qwertyui,false"})
+    void containsUpper_shouldReturnTrue_WhenPasswordUpperLetterHas(String txt, boolean check){
+        assertEquals(PasswordValidator.containsUpper(txt),check);}
+
+    @ParameterizedTest
+    @CsvSource ({"password,true",
+            "admin,true",
+            "GSd21$@1gg_?s,false",
+            "aDDm11n$,false",
+            "GSd21$@1gg_?s,false"})
     void isCommonPassword_shouldReturnTrue_WhenPasswordCommonIs(String txt, boolean check){
         assertEquals(PasswordValidator.isCommonPassword(txt),check);}
     @ParameterizedTest
-    @CsvSource ({"GSd21$@1gg_?s","!@#$%^&*()-_+={}[]|:;'<>,.?/","True",
-                 "admin$","!@#$%^&*()-_+={}[]|:;'<>,.?/","True",
-                 "Password","!@#$%^&*()-_+={}[]|:;'<>,.?/","False"})
-    void containsSpecialChar_shouldReturnTrue_WhenPasswordSpecialCharHas(String txt, String allowed, boolean check){
-        assertEquals(PasswordValidator.containsSpecialChar(txt, allowed),check);}
-
+    @CsvSource ({"GSd21$@1gg_?s,true",
+            "admin$,true",
+            "GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "Password,false"})
+    void containsSpecialChar_shouldReturnTrue_WhenPasswordSpecialCharHas(String txt, boolean check){
+        assertEquals(PasswordValidator.containsSpecialChar(txt),check);}
+    @ParameterizedTest
+    @CsvSource ({"GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "GSd21$@1gg_?s,true",
+            "aDDm11n$,true",
+            "Password,false"})
+    void isValid_shouldReturnTrue_WhenPasswordValidIs(String txt, boolean check){
+        assertEquals(PasswordValidator.isValid(txt),check);}
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
